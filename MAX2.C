a8f6e0635650ee6e986f3df6d44bee33c19ec647
prudhvi-raj143 <prudhvi201006@gmaiil.com>
2026-07-14 12:01:09 +0530

    second

---
+++ MAX2.C
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<limits.h>
+int main()
+{
+int n;
+scanf("%d",&n);
+int a[n],max=INT_MIN,cur;
+for(int i=0;i<n;i++)
+{
+    scanf("%d",&a[i]);
+}
+for(int i=0;i<n;i++)
+{
+    for(int j=i;j<n;j++)
+    {
+        cur=0;
+        for(int k=i;k<=j;k++)
+        {
+            cur+=a[k];
+        
+        if(cur>max)
+        {
+            max=cur;
+        }
+    }
+}
+}
+printf("%d",max);
+//higit s
+return 0;
+}
